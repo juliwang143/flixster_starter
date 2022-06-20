90a55f87f4f8a258f64245c0f9e7c370e4cdb687
Julia Wang <juliawang143@fb.com>
2022-06-19 23:33:37 -0700

    trailer

--- script.js
+++ script.js
@@ -1,108 +1,3 @@
-// const APIkey = "01dd4debe44e9c69de7ca9ec4ff95794";
-// const imageBaseUrl = 'https://image.tmdb.org/t/p';
-// const url = "https://api.themoviedb.org/3/movie/now_playing?api_key=";
-// const searchUrl = "https://api.themoviedb.org/3/search/movie?api_key=";
-
-// var page = 1;
-// var searchValue = '';
-
-// const containerElem = document.getElementById('container');
-// const movieGridElem = document.querySelector('#movies-grid');
-// const searchInputElem = document.querySelector('#search-input');
-// const loadBtnElem = document.querySelector('#load-more-movies-btn');
-// const clearBtnElem = document.querySelector('#clear-btn');
-// const nowPlayingElem = document.getElementById('now-playing-header');
-
-// // initally render page
-// getMovies();
-
-// async function getMovies() {
-//     console.log('getting movies');
-//     const response = await fetch(`${url}${APIkey}&page=${page}`);
-//     const jsonResponse = await response.json();
-//     const data = jsonResponse.results;
-
-//     // render
-//     data.forEach(element => {
-//         const movie = {title: element.title, voteAverage: element.vote_average, posterPath: element.poster_path};
-//         console.log('movie: ' + JSON.stringify(movie));
-//         loadMovie(imageBaseUrl, movie);
-//     });
-// }
-
-// async function getSearchResults(searchParam) {
-
-//     // working    
-//     const response = await fetch(`${searchUrl}${APIkey}&query=${searchParam}&page=${page}`);
-//     const jsonResponse = await response.json();
-//     const data = jsonResponse.results;
-
-//     console.log('data: ' + JSON.stringify(data));
-//     // render
-//     data.forEach(element => {
-//         const movie = {title: element.title, voteAverage: element.vote_average, posterPath: element.poster_path};
-//         console.log('movie: ' + JSON.stringify(movie));
-//         loadMovie(imageBaseUrl, movie);
-//     });
-// }
-
-// loadBtnElem.addEventListener("click", function () {
-//     page++;
-//     if (searchValue != '') {
-//         getSearchResults(searchValue);
-//     } else {
-//         getMovies();
-//     }
-//     searchInputElem.value = '';
-// });
-
-// searchInputElem.addEventListener("keyup", (e) => {
-//     if (e.which === 13) {
-//         //The keycode for enter key is 13
-
-//         // remove "now playing" header
-//         nowPlayingElem.classList.add('hidden');
-
-//         // clear gifResults
-//         page = 1;
-//         movieGridElem.innerHTML = ``;
-
-//         searchValue = searchInputElem.value;
-//         getSearchResults(searchInputElem.value);
-//         // reset search bar
-//         searchInputElem.value = '';
-
-//         // make "clear search" button visible
-//         clearBtnElem.classList.remove('hidden');
-//     }
-// });
-
-// function loadMovie(imageBaseUrl, movie) {
-//     movieGridElem.innerHTML += `
-//         <div class="movie-card">
-//             <div class="movie-title-wrapper">
-//                 <span class="movie-title text">${movie.title}</span>
-//             </div>
-//             <img class="movie-poster" src="${imageBaseUrl}/w342${movie.posterPath}" alt="${movie.title}" title="${movie.title}"/>
-//             <div class="movie-title-wrapper">
-//                 <span class="text">${movie.voteAverage}</span>
-//             </div>
-//         </div>
-//     `
-// }
-
-// clearBtnElem.addEventListener("click", function () {
-//     page = 1;
-//     searchValue = '';
-//     clearBtnElem.classList.add('hidden');
-//     nowPlayingElem.classList.remove('hidden');
-//     movieGridElem.innerHTML = ``;
-//     getMovies();
-// });
-
-
-
-
 const APIkey = "01dd4debe44e9c69de7ca9ec4ff95794";
 const imageBaseUrl = 'https://image.tmdb.org/t/p';
 const url = "https://api.themoviedb.org/3/movie/now_playing?api_key=";
@@ -120,25 +15,16 @@ const clearBtnElem = document.querySelector('#clear-btn');
 const nowPlayingElem = document.getElementById('now-playing-header');
 const searchResultsLabelElem = document.getElementById('search-results-label');
 
-// added
-// keep track of state of popup and previous container html code
-// var containerCode = '';
-// added
-
 // initally render page
 getMovies();
 
 async function getMovies() {
-    console.log('getting movies');
     const response = await fetch(`${url}${APIkey}&page=${page}`);
     const jsonResponse = await response.json();
     const data = jsonResponse.results;
 
-    console.log('data: ' + JSON.stringify(data));
     // render
     data.forEach(element => {
-        // const movie = {title: element.title, voteAverage: element.vote_average, posterPath: element.poster_path};
-        // console.log('movie: ' + JSON.stringify(movie));
         loadMovie(imageBaseUrl, element);
     });
 }
@@ -150,11 +36,8 @@ async function getSearchResults(searchParam) {
     const jsonResponse = await response.json();
     const data = jsonResponse.results;
 
-    console.log('data: ' + JSON.stringify(data));
     // render
     data.forEach(element => {
-        // const movie = {title: element.title, voteAverage: element.vote_average, posterPath: element.poster_path};
-        // console.log('movie: ' + JSON.stringify(movie));
         loadMovie(imageBaseUrl, element);
     });
 }
@@ -182,8 +65,6 @@ searchInputElem.addEventListener("keyup", (e) => {
 
         searchValue = searchInputElem.value;
         getSearchResults(searchInputElem.value);
-        // reset search bar
-        // searchInputElem.value = '';
 
         // make "clear search" button visible
         clearBtnElem.classList.remove('hidden');
@@ -191,13 +72,37 @@ searchInputElem.addEventListener("keyup", (e) => {
         // add search results label
         searchResultsLabelElem.classList.remove('hidden');
         searchResultsLabelElem.innerHTML = `"${searchValue}" results`;
-        console.log('hiii: ' + searchResultsLabelElem.value);
     }
 });
 
 function loadMovie(imageBaseUrl, movie) {
     const movieCard = document.createElement("div");
     movieCard.className = "movie-card";
+
+    // console.log('movie poster path: ' + JSON.stringify(movie));
+    // // added
+    // if (movie.poster_path !== null) {
+    //     movieCard.innerHTML += `
+    //         <img class="movie-poster" src="${imageBaseUrl}/w342${movie.poster_path}" alt="${movie.title}" title="${movie.title}"/>
+    //     `;
+    // } else {
+    //     movieCard.innerHTML += `
+    //         <img class="movie-poster" alt="Image not available" title="${movie.title}"/>
+    //     `;
+    // }
+
+    // movieCard.innerHTML += `
+    // <div class="movie-title-wrapper">
+    //     <span class="movie-title text">${movie.title}</span>
+    // </div>
+    // <div class="movie-title-wrapper">
+    //     <span class="text">Rating: ${movie.vote_average}/10</span>
+    // </div>
+    // `;
+
+    // movieGridElem.appendChild(movieCard); 
+
+    // working
     movieCard.innerHTML = `
     <img class="movie-poster" src="${imageBaseUrl}/w342${movie.poster_path}" alt="${movie.title}" title="${movie.title}"/>
     <div class="movie-title-wrapper">
@@ -208,12 +113,9 @@ function loadMovie(imageBaseUrl, movie) {
     </div>
     `;
 
-    movieGridElem.appendChild(movieCard);
+    movieGridElem.appendChild(movieCard); 
 
     movieCard.addEventListener("click", function () {
-        // containerElem.style.opacity = 0.2;
-        console.log('click');
-
         const closePopupButton = document.createElement('button');
         closePopupButton.className = 'close-popup';
         closePopupButton.addEventListener("click", () => {
@@ -222,15 +124,34 @@ function loadMovie(imageBaseUrl, movie) {
 
         const popup = document.createElement('div');
         popup.className = 'popup';
-        popup.innerHTML = `
-        <div class="popup-movie-info-wrapper">
+
+        const popupWrapper = document.createElement('div');
+        popupWrapper.className = 'popup-movie-info-wrapper';
+        popupWrapper.innerHTML = `
             <span class="popup-movie-title text">${movie.title}</span>
             <span class="text">Rating: ${movie.vote_average}/10</span>
-            <span class="text">Overview: ${movie.overview}</span>
-        </div>
+            <span class="text overview">Overview: ${movie.overview}</span>
         `;
 
-        popup.appendChild(closePopupButton);
+        if (movie.video == false) {
+            popupWrapper.innerHTML += `
+            <img class="movie-poster" src="${imageBaseUrl}/w342${movie.poster_path}" alt="${movie.title}" title="${movie.title}"/>
+            `;
+        } else {
+            getTrailer(movie).then((trailerKey) => {
+                popupWrapper.innerHTML += `
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/${trailerKey}" 
+                        title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+                        clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                `;
+                popupWrapper.appendChild(closePopupButton);
+                popup.appendChild(popupWrapper);
+                document.body.appendChild(popup);
+            });
+        }
+
+        popupWrapper.appendChild(closePopupButton);
+        popup.appendChild(popupWrapper);
         document.body.appendChild(popup);
     }); 
 }
@@ -246,16 +167,11 @@ clearBtnElem.addEventListener("click", function () {
     getMovies();
 });
 
-async function getTrailer() {
-    const response = await fetch(`${videourl}${APIkey}&page=${page}`);
+async function getTrailer(movie) {
+    const response = await fetch(`${videoUrl}${movie.id}/videos?api_key=${APIkey}&language=en-US`);
     const jsonResponse = await response.json();
     const data = jsonResponse.results;
-
-    console.log('video: ' + JSON.stringify(data));
-    if (data.length == 0) {
-        // TODO return image poster path
-        return 'No Trailer';
-    } else {
-        return data[0].key;
-    }
+    const firstIndex = data[0];
+    const key = firstIndex.key;
+    return key;
 }
